c695b76a98fb9fd0e92f5e4070a93556426896fe
AhmedAshmaig <111660066+AhmedAshmaig@users.noreply.github.com>
2023-12-02 23:03:22 -0800

    test3

--- Project1.py
+++ Project1.py
@@ -3,3 +3,4 @@ first_name = input("What is your first name?\n")
 last_name = input("What is your last name?\n")
 print("Your name is " + first_name + last_name) 
 #test
+#print("Your name is " + first_name + last_name) 
