5ce5ace57555a09a957e3e562dfa75e12b80abb1
AhmedAshmaig <111660066+AhmedAshmaig@users.noreply.github.com>
2023-12-02 23:02:37 -0800

    test2

--- Project1.py
+++ Project1.py
@@ -1,4 +1,5 @@
 print("Welcome to the name generator")
 first_name = input("What is your first name?\n")
 last_name = input("What is your last name?\n")
-print("Your name is " + first_name + last_name)
+print("Your name is " + first_name + last_name) 
+#test
